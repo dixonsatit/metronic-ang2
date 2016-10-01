2a53bb0f6f63ac296eb234c33b1739413eaa56d0
Sathit Seethaphon <dixonsatit@gmail.com>
2016-10-01 22:07:30 +0700

    update part template

--- www/app/new.component.js
+++ www/app/new.component.js
@@ -1,8 +1,12 @@
 "use strict";
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    var c = arguments.length,
+        r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc,
+        d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    else
+        for (var i = decorators.length - 1; i >= 0; i--)
+            if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 var __metadata = (this && this.__metadata) || function (k, v) {
@@ -38,8 +42,8 @@ var NewComponent = (function () {
     NewComponent = __decorate([
         core_1.Component({
             selector: 'new-page',
-            templateUrl: '../templates/new.component.html'
-        }), 
+            templateUrl: 'metronic-ang2/www/templates/new.component.html'
+        }),
         __metadata('design:paramtypes', [connection_1.Connection, role_service_1.RoleService, router_1.Router])
     ], NewComponent);
     return NewComponent;
